b24125c52605370263ad56cc06076ee8019a0b3f
Moshe Nahmias <moshegrey@gmail.com>
2020-11-30 13:27:28 +0200

    fix name of dir name variable

--- resume_json/resume-cli.py
+++ resume_json/resume-cli.py
@@ -18,7 +18,7 @@ def main():
                         default='resume.json')
     parser.add_argument('-p', '--port', metavar='port', type=str)
     parser.add_argument('-s', '--silent', metavar='True/False', type=bool, default=False)
-    parser.add_argument('-d', '--dir', metavar='path', default='public')
+    parser.add_argument('-d', '--dir', metavar='path', default=os.getcwd())
     parser.add_argument('--schema', metavar='relative/path')
     parser.add_argument('--init', action='store_true')
     parser.add_argument('-v', '--validate', metavar='file/to/validate')
@@ -26,11 +26,11 @@ def main():
     parser.add_argument('-S', '--serve', action='store_true')
     parser.add_argument('-l', '--language', metavar='language code', default='en')
 
-    cwd = os.getcwd()
     args = parser.parse_args()
+    file_path = args.dir
     resume_json = ResumeJson()
     if args.init:
-        resume_json.create(cwd, args.resume)
+        resume_json.create(file_path, args.resume)
     elif args.validate:
         schema = args.schema
         result = resume_json.validate(args.validate, schema)
@@ -41,9 +41,9 @@ def main():
             print(f'The path to the errors on the json is (there could be more):')
             print(f'Error: {result}')
     elif args.export:
-        resume_json.export(cwd, args.resume, args.export, args.theme, args.format, args.language)
+        resume_json.export(file_path, args.resume, args.export, args.theme, args.format, args.language)
     elif args.serve:
-        resume_json.serve(cwd, args.resume, args.language)
+        resume_json.serve(file_path, args.resume, args.language)
 
 
 if __name__ == '__main__':
