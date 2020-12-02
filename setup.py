321f36409c70d7cffd2d1bcc6924aa14d68e44b5
Moshe Nahmias <moshegrey@gmail.com>
2020-12-03 00:21:02 +0200

    bump version and fix setup

--- setup.py
+++ setup.py
@@ -27,17 +27,21 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/moshe742/resume-json-python',
-    license=LICENSE,
     include_package_data=True,
     python_requires='>=3.6',
     project_urls={
         'bug_tracker': 'https://github.com/moshe742/resume-json-python/issues',
-        'documentation': ''
+        # 'documentation': '',
     },
     entry_points={
         'console_scripts': [
             'resume_cli=resume_json.resume_cli:main',
         ]
     },
-    install_requires=['Jinja2', 'jsonschema', 'requests', 'WeasyPrint', 'cherrypy']
+    install_requires=['Jinja2', 'jsonschema', 'requests', 'WeasyPrint', 'cherrypy'],
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities'
+    ]
 )
